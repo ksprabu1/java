5766732702e3f059ce36f7d58df524815bd0ceba
Seenivasaprabu <125130074+ksprabu1@users.noreply.github.com>
2023-03-02 14:57:10 +0530

    First Commit

---
+++ src/com/javaprograms/Factorial.java
@@ -0,0 +1,19 @@
+package com.javaprograms;
+
+import java.util.Iterator;
+
+public class Factorial {
+
+	public static void main(String[] args) {
+		int i , fact=1;
+		
+		int number =8;
+		
+		for (i = 1; i <=number; i++) {
+			fact=fact=i;
+		}
+		System.out.println(fact);
+	}
+	
+	
+}
